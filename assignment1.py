5640d39465b5b075bae4b2c3ed1f5f9ba65a11eb
Jeroen Overschie <jeroenoverschie@gmail.com>
2019-11-21 18:09:03 +0100

    Finish initialization

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,20 @@
 import numpy as np
 
-# Draw 1000 random samples from Gaussian distribution.
-mu, sigma = 0, 0.1
-s = np.random.normal(mu, sigma, 1000)
+a = 1 # <to be implememented>
+N = 20 # Amount of features
+P = a*N # Amount of samples
+
+xi = []
+S = []
+for i in range(P):
+    # Draw N random samples from Gaussian distribution.
+    mu, sigma = 0, 0.1
+    s = np.random.normal(mu, sigma, N)
+    xi.append(s)
+
+    label = np.random.randint(2)
+    S.append(label)
+
+w = 0
+
+print('End')
