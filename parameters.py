0cf6dc6a954a6a90cb5f957234d2fec34df2348e
dunnkers <jeroenoverschie@gmail.com>
2019-12-12 18:03:43 +0100

    💠

--- parameters.py
+++ parameters.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 # Perceptron parameters
-aincr = 200     # alpha increment level
+aincr = 100     # alpha increment level
 nmax = 100      # max number of epochs - before stopping without convergence
-N = 1000        # amount of features - to use for the dataset
-nd = 50         # number of datasets - to generate for each alpha
+N = 5           # amount of features - to use for the dataset
+nd = 200        # number of datasets - to generate for each alpha
 
 # Spawn values of alpha
 alphas = np.linspace(0.75, 3, aincr)
 
 # Files
-resfn = './nmax={},N={},nd={},aincr={}.pickle'
-    .format(nmax, N, nd, aincr) # results filename
+resfn = './nmax={},N={},nd={},aincr={}.pickle'.format( # results filename
+    nmax, N, nd, aincr
+)
