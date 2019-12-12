78e01bebf790c9e27cb76a1fb7328fe95bc3c2c9
zhivkoplias <homo.korvin@gmail.com>
2019-12-12 17:17:23 +0100

    jupyter-notebook-plot

--- plotting_with_jupyter.py
+++ plotting_with_jupyter.py
@@ -1,11 +1,19 @@
+#plot for basic part
+
+nmax = 100      # max number of epochs - before stopping without convergence
+N = 20          # amount of features - to use for the dataset
+nd = 50     # number of datasets - to generate for each alpha
+
+alphas = np.linspace(0.75, 3, 30)
+
 alphas = np.linspace(0.75, 3, 10)
 
 convergence_av_basic = np.empty([0,10])
 
 for i in alphas:
     convergence = np.empty([0, 1])
-    for j in range(50):
-        convergence = np.append(convergence,(perceptron(i, 20, 100)))
+    for j in range(nd):
+        convergence = np.append(convergence,(perceptron(i, N, nmax)))
     convergence_av_basic = np.append(convergence_av_basic,np.mean(convergence))
 
 plt.figure(figsize=(20,10))
